b2a2a6960f47ed171e41da5b3329a72bd2bae67b
Iliyan-0512 <62907829+Iliyan-0512@users.noreply.github.com>
2023-10-27 20:30:21 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+Console.WriteLine("Hello");
+int myHeroHealt = 100;
+int myAttack = 10;
+int enemyHealt = 100;
+int enemyAttack = 5;
+List<int> tokens = Console.ReadLine().Split().Select(int.Parse).ToList();
+while (myHeroHealt>0)
+{
+    string command=Console.ReadLine();
+    command =
+}
+
